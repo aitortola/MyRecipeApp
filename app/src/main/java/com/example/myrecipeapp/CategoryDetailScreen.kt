1f64fa3504bf7f379ec597f18b62241feabf88a9
Aitor Tola <a.tola@kirol.es>
2024-08-09 13:01:40 +0200

    Correccion de app

--- app/src/main/java/com/example/myrecipeapp/CategoryDetailScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/CategoryDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -26,7 +27,7 @@ fun CategoryDetailScreen(category: Category) {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
             contentDescription = "${category.strCategory} Thumbnail",
-            modifier = Modifier.fillMaxSize().aspectRatio(1f)
+            modifier = Modifier.wrapContentSize().aspectRatio(1f)
         )
         Text(
             text = category.strCategoryDescription,
